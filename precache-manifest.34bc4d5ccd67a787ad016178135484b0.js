c64e04447f78f4b673bfe23a4dc2f34a9de0ef33
Das <maccameron@live.ca>
2020-01-13 23:45:16 -0700

    Updates

---
+++ precache-manifest.34bc4d5ccd67a787ad016178135484b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ce18cc6f6cd64849e2145c02a2e443",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "2895bb33d838a1f90fa2",
+    "url": "/rolodex/static/css/main.b3ec8965.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2895bb33d838a1f90fa2",
+    "url": "/rolodex/static/js/main.8803119e.chunk.js"
+  },
+  {
+    "revision": "50b548b557b4dfff29ea",
+    "url": "/rolodex/static/js/runtime-main.3d4c2e81.js"
+  }
+]);
